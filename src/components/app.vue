742579c2b7e0ebe2af48d2b2b2bdda722036fc80
Dominik Sasko <dominik.sasko@asial.co.jp>
2022-04-26 13:12:35 +0900

    first commit

---
+++ src/components/app.vue
@@ -0,0 +1,58 @@
+<template>
+  <f7-app v-bind="f7params">
+    <!-- Your main view, should have "view-main" class -->
+    <f7-view main class="safe-areas" url="/"></f7-view>
+  </f7-app>
+</template>
+<script>
+import { onMounted } from "vue";
+import { f7, f7ready } from "framework7-vue";
+import { getDevice } from "framework7/lite-bundle";
+import helperFunctions from "../js/helperFunctions";
+
+import routes from "../js/routes.js";
+import store from "../js/store";
+
+export default {
+  setup() {
+    const device = getDevice();
+    // Framework7 Parameters
+    const f7params = {
+      name: "Piano Chords AI Recognition", // App name
+      theme: "auto", // Automatic theme detection
+
+      id: "io.framework7.myapp", // App bundle ID
+      // App store
+      store: store,
+      // App routes
+      routes: routes,
+
+      // Input settings
+      input: {
+        scrollIntoViewOnFocus: device.cordova && !device.electron,
+        scrollIntoViewCentered: device.cordova && !device.electron,
+      },
+      // Cordova Statusbar settings
+      statusbar: {
+        iosOverlaysWebView: true,
+        androidOverlaysWebView: false,
+      },
+    };
+
+    onMounted(() => {
+      f7ready(async () => {
+        await helperFunctions.loadModel();
+
+        f7.views.current.router.navigate("/predicting", {
+          transition: "f7-dive",
+          clearPreviousHistory: true,
+        });
+      });
+    });
+
+    return {
+      f7params,
+    };
+  },
+};
+</script>
---
+++ src/js/helperFunctions.js
@@ -0,0 +1,74 @@
+const laptopModelURL =
+  "https://teachablemachine.withgoogle.com/models/oQapXjh1t/";
+const phoneModelURL =
+  "https://teachablemachine.withgoogle.com/models/6Oapb3YVQ/";
+let predictedChord;
+let classifier;
+
+const isAndroid = () => {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  if (/android/i.test(userAgent)) return true;
+  return false;
+};
+
+const isIos = () => {
+  const userAgent = navigator.userAgent || navigator.vendor || window.opera;
+  if (/iPad|iPhone|iPod/i.test(userAgent)) return true;
+  return false;
+};
+
+const isMobile = () => {
+  if (window.cordova && (isAndroid() || isIos())) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+const loadModel = async () => {
+  let currentModelURL = isAndroid ? phoneModelURL : laptopModelURL;
+  const options = { probabilityThreshold: 0.8 };
+
+  if (isMobile() == true) {
+    document.addEventListener("deviceready", async () => {
+      cordova.plugins.diagnostic.requestMicrophoneAuthorization(
+        (status) => {
+          if (status === cordova.plugins.diagnostic.permissionStatus.GRANTED) {
+            console.log("Microphone use is authorized");
+          }
+        },
+        (error) => {
+          console.error(error);
+        }
+      );
+    });
+  }
+
+  classifier = await ml5.soundClassifier(
+    currentModelURL + "model.json",
+    options
+  );
+};
+
+const startPrediction = async (callback) => {
+  classifier.classify(gotResults);
+
+  function gotResults(error, results) {
+    if (error) {
+      console.error(error);
+      return;
+    }
+    callback(results[0].label);
+  }
+};
+
+const helperFunctions = {
+  loadModel,
+  isAndroid,
+  isIos,
+  isMobile,
+  startPrediction,
+  predictedChord,
+};
+
+export default helperFunctions;
